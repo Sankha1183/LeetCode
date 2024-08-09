272c36cfeeaa6baf68a871d5cc40ddad8d450b13
Sankha1183 <www.sankha09@gmail.com>
2024-08-09 14:32:47 +0530

    Time: 0 ms (100%), Space: 41.3 MB (40.98%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -1,35 +1,33 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-    int[][] matrix=new int[n][n];
-    int top=0,bottom=n-1;
-    int left=0,right=n-1;
-    int a=1;
-    while(top<=bottom && left<=right){
-        for(int i=left;i<=right;i++){
-            matrix[top][i]=a;
-            a++;
-        } top++;
-        for(int i=top;i<=bottom;i++){
-            matrix[i][right]=a;
-            a++;
-        }
-        right--;
-        if(top<=bottom){
-            for(int i=right;i>=left;i--){
-                matrix[bottom][i]=a;
-                a++;
+        int[][] matrix = new int[n][n];
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int val = 1;
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = val++;
             }
-            bottom--;
-        }
-        if(left<=right){
-            for(int i=bottom;i>=top;i--){
-                matrix[i][left]=a;
-                a++;
+            for (int i = top + 1; i <= bottom; i++) {
+                matrix[i][right] = val++;
+
             }
-            left++;
+            if (top <= bottom && left <= right) {
+                for (int i = right - 1; i >= left; i--) {
+                    matrix[bottom][i] = val++;
+                }
+                for (int i = bottom - 1; i > top; i--) {
+                    matrix[i][left] = val++;
+                }
+               
+            }
+                top++;
+                bottom--;
+                left++;
+                right--;
+
         }
-    }
-    return matrix;
-        
+        return matrix;
+
     }
 }
