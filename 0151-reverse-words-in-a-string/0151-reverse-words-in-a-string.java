6ca7bca5434727022866df948bfb2cb3d7766353
Sankhadeep Chakraborty <www.sankha09@gmail.com>
2024-08-22 21:27:25 +0530

    Time: 2 ms (99.61%), Space: 42.5 MB (95%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,49 +1,31 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        sb.reverse();  // Reverse the entire string
-        int i = 0;
-        int l = 0, r = 0;
-        int n = sb.length();
-
-        while (i < n) {
-            
-            while (i < n && sb.charAt(i) == ' ') {
-                i++;
-            }
-
-            
-            if (r < n && i < n && sb.charAt(i) != ' ') {
-                l = r;
-                while (i < n && sb.charAt(i) != ' ') {
-                    sb.setCharAt(r++, sb.charAt(i++));
+        char[] arr=s.toCharArray();
+        int n=arr.length;
+        reverse(arr,0,n-1);
+        int r=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=' '){
+                if(r!=0) arr[r++]=' ';
+                int l=r;
+                while(i<n && arr[i]!=' '){
+                    arr[r++]=arr[i++];
                 }
-                reverse(sb, l, r - 1); 
-            }
-
-            
-            if (i < n) {
-                sb.setCharAt(r++, ' ');
+                reverse(arr,l,r-1);
             }
         }
+        return new String(arr,0,r);
 
         
-        if (r > 0 && sb.charAt(r - 1) == ' ') {
-            sb.setLength(r - 1);
-        } else {
-            sb.setLength(r);
-        }
-
-        return sb.toString();
     }
-
-    private void reverse(StringBuilder sb, int l, int r) {
-        while (l < r) {
-            char temp = sb.charAt(l);
-            sb.setCharAt(l, sb.charAt(r));
-            sb.setCharAt(r, temp);
-            l++;
-            r--;
+    private void reverse(char[] arr,int l,int r){
+        while(l<r){
+        char temp=arr[l];
+        arr[l]=arr[r];
+        arr[r]=temp;
+        l++;
+        r--;
         }
+       
     }
 }
