100f5410468c37764497801cff5dd7621088a9fd
Sankhadeep Chakraborty <www.sankha09@gmail.com>
2024-08-24 17:03:41 +0530

    Time: 2 ms (99.59%), Space: 42.5 MB (95.07%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,34 +1,31 @@
 class Solution {
     public String reverseWords(String s) {
-        int left=0;
-        int right=s.length()-1;
-        String temp="";
-        String ans="";
-        while(left<=right){
-            char ch=s.charAt(left);
-            if(ch!=' '){
-                temp+=ch;
-            }
-            else {
-                if(!temp.equals("")){
-                if(!ans.equals("")){
-                    ans=temp+" "+ans;
+        char[] arr=s.toCharArray();
+        int n=arr.length;
+        reverse(arr,0,n-1);
+        int r=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=' '){
+                if(r!=0) arr[r++]=' ';
+                int l=r;
+                while(i<n && arr[i]!=' '){
+                    arr[r++]=arr[i++];
                 }
-            else{
-                ans=temp;
-            }
-            temp="";
-            }
-            }
-            left++;
-        }
-        if(!temp.equals("")){
-            if(!ans.equals("")){
-                ans=temp+" "+ans;
-            }else{
-                ans=temp;
+                reverse(arr,l,r-1);
             }
         }
-        return ans;
+        return new String(arr,0,r);
+
+        
+    }
+    private void reverse(char[] arr,int l,int r){
+        while(l<r){
+        char temp=arr[l];
+        arr[l]=arr[r];
+        arr[r]=temp;
+        l++;
+        r--;
+        }
+       
     }
 }
