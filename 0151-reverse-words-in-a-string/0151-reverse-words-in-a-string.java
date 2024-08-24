7acff4e4fcf94ef1a5781c4371e8e555cf81972c
Sankhadeep Chakraborty <www.sankha09@gmail.com>
2024-08-24 16:58:55 +0530

    Time: 17 ms (10.23%), Space: 45.2 MB (15.79%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,49 +1,34 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        sb.reverse();  // Reverse the entire string
-        int i = 0;
-        int l = 0, r = 0;
-        int n = sb.length();
-
-        while (i < n) {
-            
-            while (i < n && sb.charAt(i) == ' ') {
-                i++;
+        int left=0;
+        int right=s.length()-1;
+        String temp="";
+        String ans="";
+        while(left<=right){
+            char ch=s.charAt(left);
+            if(ch!=' '){
+                temp+=ch;
             }
-
-            
-            if (r < n && i < n && sb.charAt(i) != ' ') {
-                l = r;
-                while (i < n && sb.charAt(i) != ' ') {
-                    sb.setCharAt(r++, sb.charAt(i++));
+            else {
+                if(!temp.equals("")){
+                if(!ans.equals("")){
+                    ans=temp+" "+ans;
                 }
-                reverse(sb, l, r - 1); 
+            else{
+                ans=temp;
             }
-
-            
-            if (i < n) {
-                sb.setCharAt(r++, ' ');
+            temp="";
+            }
+            }
+            left++;
+        }
+        if(!temp.equals("")){
+            if(!ans.equals("")){
+                ans=temp+" "+ans;
+            }else{
+                ans=temp;
             }
         }
-
-        
-        if (r > 0 && sb.charAt(r - 1) == ' ') {
-            sb.setLength(r - 1);
-        } else {
-            sb.setLength(r);
-        }
-
-        return sb.toString();
-    }
-
-    private void reverse(StringBuilder sb, int l, int r) {
-        while (l < r) {
-            char temp = sb.charAt(l);
-            sb.setCharAt(l, sb.charAt(r));
-            sb.setCharAt(r, temp);
-            l++;
-            r--;
-        }
+        return ans;
     }
 }
