e7ca3c73ab1e3e57f0824e4084a43a657b1a7663
Sankhadeep Chakraborty <www.sankha09@gmail.com>
2024-08-29 20:43:42 +0530

    Time: 0 ms (100%), Space: 42.8 MB (9.31%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,32 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode temp1 = list1;
-        ListNode temp2 = list2;
-        ArrayList<Integer> list = new ArrayList<>();
+        if(list1==null) return list2;
+        if(list2==null) return list1;
 
-        while (temp1 != null) {
-            list.add(temp1.val);
-            temp1 = temp1.next;
+        ListNode dummy=new ListNode(-1);
+        ListNode curr=dummy;
+
+        while(list1!=null && list2!=null){
+            if(list1.val<=list2.val){
+                curr.next=list1;
+                list1=list1.next;
+            }else{
+                curr.next=list2;
+                list2=list2.next;
+            }
+            curr=curr.next;
         }
-
-        while (temp2 != null) {
-            list.add(temp2.val);
-            temp2 = temp2.next;
-        }
-
-        Collections.sort(list);
-        ListNode head = convert(list);
-        return head;
-    }
-
-    private ListNode convert(ArrayList<Integer> list) {
-        if (list.isEmpty()) return null;
-        ListNode head = new ListNode(list.get(0));
-        ListNode current = head;
-        for (int i = 1; i < list.size(); i++) {
-            current.next = new ListNode(list.get(i));
-            current = current.next;
-        }
-        return head;
+        if(list1!=null){
+            curr.next=list1;
+        }else curr.next=list2;
+        return dummy.next;
     }
 }
