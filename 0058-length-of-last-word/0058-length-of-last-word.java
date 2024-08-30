910015ba829990efe5d1032601d425f529e7b57a
Sankhadeep Chakraborty <www.sankha09@gmail.com>
2024-08-30 19:18:57 +0530

    Time: 0 ms (100%), Space: 41.6 MB (66.92%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -2,15 +2,19 @@ class Solution {
     public int lengthOfLastWord(String s) {
         int n = s.length();
         int count = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            char ch = s.charAt(i);
-            if (ch != ' ') {
-                count += 1;
-            }
-            else if (count>0) {
-                break;
-            }
+        
+        // Start from the end of the string and skip any trailing spaces
+        int i = n - 1;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
         }
+        
+        // Count the length of the last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            count++;
+            i--;
+        }
+        
         return count;
     }
 }
