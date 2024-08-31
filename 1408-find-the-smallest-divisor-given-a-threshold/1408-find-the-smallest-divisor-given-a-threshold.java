32a20c162822b51ec0d9d59f34565190605f8eb8
Sankhadeep Chakraborty <www.sankha09@gmail.com>
2024-08-31 13:05:47 +0530

    Time: 6 ms (98.26%), Space: 47.9 MB (67.37%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
@@ -1,36 +1,37 @@
 class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
-        int n=nums.length;
-        int low=1;
-        int high=getMax(nums);
-        int ans=-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(sumOfD(nums,mid)<=threshold){
-                ans=mid;
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
+        int low = 1;
+        int high = getMax(nums);
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (sumOfD(nums, mid) <= threshold) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
+
         return ans;
+    }
+
+    private int getMax(int[] nums) {
+        int max = nums[0];
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
     }
 
     private int sumOfD(int[] nums, int divisor) {
         int sum = 0;
         for (int num : nums) {
-            sum += Math.ceil((double) num / divisor);
+            sum += (num + divisor - 1) / divisor;
         }
         return sum;
-    }
-    int getMax(int[] nums){
-            int max=nums[0];
-        for(int num:nums){
-            if(num>max){
-                max=num;
-            }
-        }
-        return max;
     }
 }
