601635f7f03ceafdb9153a8fd089899a63cf153a
Sankhadeep Chakraborty <www.sankha09@gmail.com>
2024-09-02 01:20:02 +0530

    Time: 3 ms (89.05%), Space: 44.4 MB (48.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] a1=new int[26];
         if(s.length()!=t.length()){
             return false;
         }
+        char[]c1=s.toCharArray();
+        char[]c2=t.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
         for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            a1[ch-'a']++;
-        }
-         for(int i=0;i<t.length();i++){
-            char ch=t.charAt(i);
-            a1[ch-'a']--;
-        }
-
-        for(int count:a1){
-            if(count!=0){
+            if(c1[i]!=c2[i]){
                 return false;
             }
         }
-    return true;
-
+        return true;
     }
 }
