040e296ab4614bc92cd0573e01ae72f93456d50b
Sankhadeep Chakraborty <www.sankha09@gmail.com>
2024-09-02 21:07:45 +0530

    Time: 1 ms (62.1%), Space: 41.1 MB (14.6%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> hs=new HashSet<>();
+        while(n!=1 && !hs.contains(n) ){
+            hs.add(n);
+            int sum=0;
+            while(n>0){
+            int ld=n%10;
+            sum+=ld*ld;
+            n=n/10;
+            }
+            n=sum;
+        
+        }
+        return n==1;
+        
+    }
+}
