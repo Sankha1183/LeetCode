d76a95890aebfc3ada4b91cd1973b48a6cff4227
Sankhadeep Chakraborty <www.sankha09@gmail.com>
2024-10-02 20:30:33 +0530

    Time: 4 ms (44.03%), Space: 44.1 MB (30.83%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,31 +1,26 @@
-import java.util.HashMap;
-import java.util.Stack;
-
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        // Step 1: Create a map to store the next greater element for each number in nums2
-        HashMap<Integer, Integer> map = new HashMap<>();
-        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
 
-        // Step 2: Traverse nums2 from left to right and find the next greater element for each number
-        for (int num : nums2) {
-            while (!stack.isEmpty() && stack.peek() < num) {
-                map.put(stack.pop(), num); // The top of the stack has a smaller element than num
-            }
-            stack.push(num); // Push the current number onto the stack
+        for(int i=nums2.length-1;i>=0;i--){
+            int num=nums2[i];
+        
+        while(!st.isEmpty() && st.peek()<=num){
+            st.pop();
+        }
+        if(st.isEmpty()){
+            mp.put(num,-1); 
+        }else{
+            mp.put(num, st.peek());
+        }
+        st.push(num);
         }
 
-        // Step 3: For any elements left in the stack, there is no next greater element
-        while (!stack.isEmpty()) {
-            map.put(stack.pop(), -1);
-        }
-
-        // Step 4: Build the result array based on the next greater elements for nums1
-        int[] result = new int[nums1.length];
+        int[] res = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
-            result[i] = map.get(nums1[i]); // Get the next greater element for nums1[i]
+            res[i] = mp.get(nums1[i]);
         }
-
-        return result;
+        return res;
     }
 }
