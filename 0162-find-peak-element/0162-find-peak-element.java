e5ea5597350c789c08abbf3ad4bcd409042b871c
Sankhadeep Chakraborty <www.sankha09@gmail.com>
2024-12-05 11:44:06 +0530

    Time: 0 ms (100%), Space: 42.2 MB (62.19%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -2,7 +2,7 @@ class Solution {
     public int findPeakElement(int[] nums) {
         int n = nums.length;
 
-        // Handle edge cases for single element or first/last element being a peak
+
         if (n == 1) {
             return 0;
         }
@@ -13,24 +13,24 @@ class Solution {
             return n - 1;
         }
 
-        // Binary search for peak element
+     
         int low = 0;
         int high = n - 1;
         
         while (low < high) {
             int mid = low + (high - low) / 2;
 
-            // Compare mid with its right neighbor to decide the direction
+            
             if (nums[mid] > nums[mid + 1]) {
-                // Peak lies in the left half or at mid
+                
                 high = mid;
             } else {
-                // Peak lies in the right half
+               
                 low = mid + 1;
             }
         }
 
-        // Low and high converge to the peak element index
+
         return low;
     }
 }
