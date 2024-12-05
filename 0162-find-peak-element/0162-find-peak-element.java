4b746858cdb9df17e25299557078f7aac518fc41
Sankhadeep Chakraborty <www.sankha09@gmail.com>
2024-12-05 11:43:01 +0530

    Time: 0 ms (100%), Space: 42.1 MB (86.81%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,12 +1,36 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-    int n=nums.length;
-    int res=0;
-    for(int i=0;i<n;i++){
-        if((i==0 || nums[i-1]<nums[i]) && (i==n-1 || nums[i]>nums[i+1])){
-            res=i;
+        int n = nums.length;
+
+        // Handle edge cases for single element or first/last element being a peak
+        if (n == 1) {
+            return 0;
         }
-    }
-    return res;
+        if (nums[0] > nums[1]) {
+            return 0;
+        }
+        if (nums[n - 1] > nums[n - 2]) {
+            return n - 1;
+        }
+
+        // Binary search for peak element
+        int low = 0;
+        int high = n - 1;
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            // Compare mid with its right neighbor to decide the direction
+            if (nums[mid] > nums[mid + 1]) {
+                // Peak lies in the left half or at mid
+                high = mid;
+            } else {
+                // Peak lies in the right half
+                low = mid + 1;
+            }
+        }
+
+        // Low and high converge to the peak element index
+        return low;
     }
 }
